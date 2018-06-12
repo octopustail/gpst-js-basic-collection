d62ac8e328f74f87150b83bbfe404c800b640e52
octopustail <494479914@qq.com>
2018-06-12 17:30:34 +0800

    完成s1 ,s2-1,s2-2

--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,5 +1,60 @@
 'use strict';
 
+function findObjInArr(result, str) {
+    let newObj;
+    for (let obj of result) {
+        if (obj.key === str) {
+            newObj = obj;
+        }
+    }
+    return newObj
+}
+
+function countElements(collection) {
+    let result = [];
+    for (let str of collection) {
+        let obj = findObjInArr(result, str)
+        if (obj) {
+            obj.summary++;
+        } else {
+            result.push({name: str, summary: 1})
+        }
+    }
+    console.log('result',result)
+    return result
+}
+
+function handleSpecialStr(str, transformedCollection) {
+    let addNum;
+    if (str.length === 3) {
+        addNum = str[2]
+    } else {
+        let startIndex = str.indexOf('['),
+            endIndex = str.indexOf(']'),
+            addNum = str.substring(startIndex + 1, endIndex)
+            console.log('addNum',addNum,str[0]);
+    }
+
+    for (let i = 0; i < parseInt(addNum); i++) {
+        transformedCollection.push(str[0])
+
+    }
+}
+
+function transformOriginList(collection) {
+    let transformedCollection = [];
+    for (let str of collection) {
+        if (str.length > 1) {
+            handleSpecialStr(str, transformedCollection)
+        } else {
+            transformedCollection.push(str)
+        }
+    }
+    return transformedCollection;
+}
+
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+    let transformedCollection = transformOriginList(collection);
+    return countElements(transformedCollection);
+
 }
